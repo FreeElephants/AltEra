e87aca28a722df347bad36a7bc56ac2ab968d33a
samizdam <samizdam@inbox.ru>
2015-12-20 20:21:41 +0300

    Test days diff calculation with alt scale.

--- tests/FreeElephants/AltEra/Interval/IntervalCalculatorTest.php
+++ tests/FreeElephants/AltEra/Interval/IntervalCalculatorTest.php
@@ -12,17 +12,29 @@ use FreeElephants\AltEra\AbstractCalendarUnitTestCase;
 class IntervalCalculatorTest extends AbstractCalendarUnitTestCase
 {
 
+    const SEC_IN_REAL_DAY = 60 * 60 * 24;
+
     public function testGetDaysDiff()
     {
-        $secPerDay = 60 * 60 * 24;
-
         $initialTimestamp = 0;
         $thirdDay = \DateTime::createFromFormat("Y-m-d", "1970-01-03");
         $actualTimestamp = $thirdDay->getTimestamp();
-        $scale = $secPerDay; // equals with real time units.
+        $scale = self::SEC_IN_REAL_DAY; // equals with real time units.
 
         $calc = new IntervalCalculator();
         $diff = $calc->getDaysDiff($initialTimestamp, $actualTimestamp, $scale);
         $this->assertEquals(2, $diff);
+    }
+
+    public function testGetDaysDiffWithAltScale()
+    {
+        $initialTimestamp = 0;
+        $thirdAltDayDateTime = \DateTime::createFromFormat("Y-m-d H:i:s", "1970-01-02 12:00:01", new \DateTimeZone('UTC'));
+        $actualTimestamp = $thirdAltDayDateTime->getTimestamp();
+        $scale = self::SEC_IN_REAL_DAY / 2; // half of real time units.
+
+        $calc = new IntervalCalculator();
+        $diff = $calc->getDaysDiff($initialTimestamp, $actualTimestamp, $scale);
+        $this->assertEquals(3, $diff);
     }
 }
