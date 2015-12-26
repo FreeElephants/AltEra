fad28c218c35e5fe3c73b48d9b3a27dd9f1212b8
samizdam <samizdam@inbox.ru>
2015-12-26 14:53:40 +0300

    Test with php fixtures.

--- tests/FreeElephants/AltEra/CalendarFactoryTest.php
+++ tests/FreeElephants/AltEra/CalendarFactoryTest.php
@@ -3,6 +3,7 @@
 namespace FreeElephants\AltEra;
 
 use FreeElephants\AltEra\Calendar\SeasonAwareCalendarInterface;
+use FreeElephants\AltEra\Calendar\CalendarInterface;
 
 /**
  *
@@ -14,11 +15,20 @@ class CalendarFactoryTest extends AbstractCalendarUnitTestCase
 
     const DS = DIRECTORY_SEPARATOR;
 
+    const FIXTURE_PATH = __DIR__ . self::DS . "_fixtures" . self::DS;
+
     public function testCreateFromYamlSeasonBasedCalendar()
     {
         $factory = new CalendarFactory();
         $input = $this->loadYamlFixture("season-based.yml");
         $this->assertInstanceOf(SeasonAwareCalendarInterface::class, $factory->createFromYaml($input));
+    }
+
+    public function testCreateFromArray()
+    {
+        $factory = new CalendarFactory();
+        $config = $this->loadPhpFixture("gregorian-month-based-ru.php");
+        $this->assertInstanceOf(CalendarInterface::class, $factory->createFromArray($config));
     }
 
     /**
@@ -28,7 +38,11 @@ class CalendarFactoryTest extends AbstractCalendarUnitTestCase
      */
     private function loadYamlFixture($filename)
     {
-        $fixturePath = __DIR__ . self::DS . "_fixtures" . self::DS;
-        return file_get_contents($fixturePath . $filename);
+        return file_get_contents(self::FIXTURE_PATH . $filename);
+    }
+
+    private function loadPhpFixture($filename)
+    {
+        return require self::FIXTURE_PATH . $filename;
     }
 }
---
+++ tests/FreeElephants/AltEra/_fixtures/gregorian-month-based-ru.php
@@ -0,0 +1,55 @@
+<?php
+return [
+    "name" => "Григорианский календарь",
+    "months" => [
+        [
+            "name" => "Январь",
+            "numberOfDays" => 31
+        ],
+        [
+            "name" => "Февраль",
+            "numberOfDays" => 28
+        ],
+        [
+            "name" => "Март",
+            "numberOfDays" => 31
+        ],
+        [
+            "name" => "Апрель",
+            "numberOfDays" => 30
+        ],
+        [
+            "name" => "Май",
+            "numberOfDays" => 31
+        ],
+        [
+            "name" => "Июнь",
+            "numberOfDays" => 30
+        ],
+        [
+            "name" => "Июль",
+            "numberOfDays" => 31
+        ],
+        [
+            "name" => "Август",
+            "numberOfDays" => 31
+        ],
+        [
+            "name" => "Сентябрь",
+            "numberOfDays" => 30
+        ],
+        [
+            "name" => "Октябрь",
+            "numberOfDays" => 31
+        ],
+        [
+            "name" => "Ноябрь",
+            "numberOfDays" => 30
+        ],
+        [
+            "name" => "Декабрь",
+            "numberOfDays" => 31
+        ]
+    ]
+
+];
